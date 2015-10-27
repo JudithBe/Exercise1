a3bd00326f9687c2ee7f6a7167cdf3b1e3937151
JudithBe <judith.bergemann@tum.de>
2015-10-27 20:44:16 +0100

    Update HelloWorld.java
    back to start

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -13,7 +13,6 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
-		System.out.println("Hello "+HelloUser.greetMe()+"!");
 	}
 
 }
